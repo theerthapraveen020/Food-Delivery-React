47011b72992e5c682b5988fbf92aa4231ace0e71
TheerthaPraveen9817 <theerthapraveen026@gmail.com>
2025-12-03 08:20:05 +0530

    First commit

---
+++ src/context/StoreContext.jsx
@@ -0,0 +1,62 @@
+import { createContext, useEffect, useState } from "react";
+import { food_list } from "../assets/assets";
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({
+      ...prev,
+      [itemId]: prev[itemId] ? prev[itemId] + 1 : 1,
+    }));
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => {
+      if (!prev[itemId]) return prev;
+
+      const updatedQty = prev[itemId] - 1;
+
+      if (updatedQty <= 0) {
+        const updatedCart = { ...prev };
+        delete updatedCart[itemId];
+        return updatedCart;
+      }
+
+      return {
+        ...prev,
+        [itemId]: updatedQty,
+      };
+    });
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
+    }
+
+    return totalAmount;
+  };
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount
+  };
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreContextProvider;
---
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,60 @@
+import React, { useContext } from 'react'
+import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext';
+
+const PlaceOrder = () => {
+
+  const{getTotalCartAmount}=useContext(StoreContext)
+  return (
+    <form  className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Details</p>
+
+        <div className="multi-fields">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+
+        <input type="email" placeholder="Email ID" />
+        <input type="text" placeholder="House No. / Street Name" />
+
+        <div className="multi-fields">
+          <input type="text" placeholder="City / Town" />
+          <input type="text" placeholder="State" />
+        </div>
+
+        <div className="multi-fields">
+          <input type="text" placeholder="PIN Code" />
+          <input type="text" placeholder="Country (India)" />
+        </div>
+
+        <input type="text" placeholder="Mobile Number" />
+      </div>
+
+      <div className="place-order-right">
+              <div className="cart-total">
+            <h2>Cart Totals</h2>
+            <div>
+              <div className="cart-total-details">
+                <p>Subtotal</p>
+                <p>₹{getTotalCartAmount()}</p>
+              </div>
+              <hr />
+              <div className="cart-total-details">
+                <p>Delivery Fee</p>
+                <p>₹{getTotalCartAmount()===0?0:2}</p>
+              </div>
+              <hr />
+              <div className="cart-total-details">
+                <b>Total</b>
+                <b>₹{ getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+              </div>
+            </div>
+            <button>PROCEED TO PAYMENT</button>
+          </div>
+      </div>
+    </form>
+  );
+};
+
+export default PlaceOrder
