9d13196403fe54a89f07a98c2b1b6a8e5c7aad21
TheerthaPraveen9817 <theerthapraveen026@gmail.com>
2026-01-22 10:12:31 +0530

    latest

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Header.css'
 const Header = () => {
+   const handleViewMenu = () => {
+    const menuSection = document.getElementById('explore-menu')
+    menuSection.scrollIntoView({ behavior: 'smooth' })
+  }
+
   return (
     <div className='header'>
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise.Our  mission is to satisfy your cravings and elevate your dining experience,one delicious meal at a time. </p>
-        <button>View Menu</button>
+        <button onClick={handleViewMenu}>View Menu</button>
       </div>
     </div>
   )
