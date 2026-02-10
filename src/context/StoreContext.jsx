f08b582f2ce79f2220cda90a65e072f673e73247
TheerthaPraveen9817 <theerthapraveen026@gmail.com>
2026-02-10 10:25:27 +0530

    latest changes

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useEffect, useState } from "react";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-mern-cpcg.onrender.com";
 
   const [cartItems, setCartItems] = useState({});
   const [token, setToken] = useState("");
